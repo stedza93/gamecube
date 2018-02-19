44f3f610bf692a487bea570f1984ee54438e93ae
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-19 16:09:21 +0100

    definisan event listener koji klikom na roll daje random broj i ssliku

--- app.js
+++ app.js
@@ -13,8 +13,20 @@ var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
 //console.log(dice);
-document.querySelector("#current-" + activePlayer).textContent = dice;
-var x = document.querySelector("#score-0").textContent;
+//document.querySelector("#current-" + activePlayer).textContent = dice;
+//var x = document.querySelector("#score-0").textContent;
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
 document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  var diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+});
