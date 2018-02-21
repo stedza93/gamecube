d5de46b1d92c08e83d26ee368b397d1de965e34e
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-21 11:55:06 +0100

    dodati jos new game i skratiti i popraviti kod

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ GAME RULES:
 var scores, roundScore, activePlayer, dice;
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 //console.log(dice);
 //document.querySelector("#current-" + activePlayer).textContent = dice;
 //var x = document.querySelector("#score-0").textContent;
@@ -26,7 +26,49 @@ document.querySelector(".dice").style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function() {
   dice = Math.floor(Math.random() * 6) + 1;
 
+  //display result
   var diceDOM = document.querySelector(".dice");
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
+
+  //update roundscore if rolled number wasnt 1
+  if (dice !== 1) {
+    roundScore += dice;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Next player
+    nextPlayer();
+  }
+});
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //add a current to global score
+  scores[activePlayer] += roundScore;
+
+  //update the UI with new scores
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+    //check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    nextPlayer();
+  }
 });
